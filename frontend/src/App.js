7ecb79c31e389dc1b800b975272772a05d83d28a
Tasim <118922118+TasimDev@users.noreply.github.com>
2023-10-21 19:39:53 +0300

    tasimchoo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Home, Blog, Contact } from './pages'
 
 const App = () => {
     return (
-        <div>
-            App
-        </div>
+        <Router>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={Blog} />
+            <Route path="/contact" component={Contact} />
+        </Router>
     )
 }
 
