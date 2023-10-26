b4d2d32aad5f14cb26a4fbb35fd99490175f26f4
Tasim <118922118+TasimDev@users.noreply.github.com>
2023-10-26 19:26:26 +0300

    tasimchoo

--- frontend/src/pages/Blog/Blog.jsx
+++ frontend/src/pages/Blog/Blog.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Blog = () => {
+
+
     return (
-        <div>Blog</div>
+
+        <div className='blog-page'>
+            <div className="blog-page-header container">
+                <h1>Welcome to Simo's Blog Page</h1>
+            </div>
+        
+            <div className="card-section">
+
+            </div>
+        </div>
+
     )
 }
 
